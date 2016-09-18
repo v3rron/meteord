c083c30bc4f8ee51c4e02950b8c1c7850eae4d79
Ronen Verdi <r0nv3r@gmail.com>
2016-09-18 12:46:32 -0400

    node-pre-gyp npm package added

--- binbuild/scripts/install_binbuild_tools.sh
+++ binbuild/scripts/install_binbuild_tools.sh
@@ -1,7 +1,7 @@
 set -e
 # for npm module re-building
 apt-get -y install build-essential libssl-dev git python
-npm install -g node-gyp
+npm install -g node-gyp node-pre-gyp
 # pre-install node source code for faster building
 $(npm bin -g)/node-gyp install ${NODE_VERSION}
 
